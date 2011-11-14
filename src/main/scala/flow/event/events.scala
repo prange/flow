e4177637c930bc693a733938f6675341d73f2978
Atle Osmoen-Prange <aop@fourspark.no>
2011-11-14 12:55:46 +0100

    Lagt til TimerEvents

--- src/main/scala/flow/event/events.scala
+++ src/main/scala/flow/event/events.scala
@@ -12,8 +12,18 @@ case class EventChain( events : List[XmlEvent], data : Group[Elem] ) {
 	def select( property : Selector[Elem] ) = data \ property \ text headOption
 }
 
+trait TimerEvent{
+	val time:DateTime
+}
 
 
+
+case class SecondTimer(time:DateTime) extends TimerEvent
+case class MinuteTimer(time:DateTime) extends TimerEvent
+case class HourTimer(time:DateTime) extends TimerEvent
+case class DayTimer(time:DateTime) extends TimerEvent
+case class WeekTimer(time:DateTime) extends TimerEvent
+case class MonthTimer(time:DateTime) extends TimerEvent
 
 object EventChain {
 
