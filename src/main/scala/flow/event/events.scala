2605aefcd37c5898f10e1cf4123e9f1159beb162
Atle Osmoen-Prange <aop@fourspark.no>
2011-11-14 15:25:03 +0100

    + Interval på EventChain, og events for opprette avansere og avslutte prosesser

--- src/main/scala/flow/event/events.scala
+++ src/main/scala/flow/event/events.scala
@@ -10,36 +10,32 @@ case class XmlEvent( eventTime : DateTime, eventType : String, data : Group[Elem
 	def select( property : Selector[Elem] ) = data \ property \ text headOption
 }
 
-case class EventChain( id:String,events : NonEmptyList[XmlEvent], data : Group[Elem] ) {
+case class EventChain( id : String, events : NonEmptyList[XmlEvent], data : Group[Elem] ) {
 	def ::( event : XmlEvent ) = EventChain( id, event <:: events, data )
 	def select( property : Selector[Elem] ) = data \ property \ text headOption
-	def interval = if(events.tail.size==0) new Interval(events.head.eventTime.getMillis(),events.head.eventTime.getMillis()+1) else (events.head.eventTime, events.tail.last.eventTime)
+	def interval = if ( events.tail.size == 0 ) new Interval( events.head.eventTime.getMillis(), events.head.eventTime.getMillis() + 1 ) else ( events.head.eventTime, events.tail.last.eventTime )
 }
 
 object EventChain {
-	
-	def from(idExtractor:XmlEvent=>String, event : XmlEvent ) = EventChain( idExtractor(event),event.wrapNel, Group() )
+
+	def from( idExtractor : XmlEvent ⇒ String, event : XmlEvent ) = EventChain( idExtractor( event ), event.wrapNel, Group() )
 }
 
-
-trait TimerEvent{
-	val time:DateTime
+trait TimerEvent {
+	val time : DateTime
 }
 
-
-
-case class SecondTimer(time:DateTime) extends TimerEvent
-case class MinuteTimer(time:DateTime) extends TimerEvent
-case class HourTimer(time:DateTime) extends TimerEvent
-case class DayTimer(time:DateTime) extends TimerEvent
-case class WeekTimer(time:DateTime) extends TimerEvent
-case class MonthTimer(time:DateTime) extends TimerEvent
-
+case class SecondTimer( time : DateTime ) extends TimerEvent
+case class MinuteTimer( time : DateTime ) extends TimerEvent
+case class HourTimer( time : DateTime ) extends TimerEvent
+case class DayTimer( time : DateTime ) extends TimerEvent
+case class WeekTimer( time : DateTime ) extends TimerEvent
+case class MonthTimer( time : DateTime ) extends TimerEvent
 
 trait ProcessEvent
-case class ProcessStartedEvent( timstamp:DateTime, event : EventChain ) extends ProcessEvent
-case class ProcessAdvancedEvent(timestamp:DateTime,  event : EventChain ) extends ProcessEvent
-case class ProcessEndedEvent( timestamp:DateTime, event : EventChain ) extends ProcessEvent
+case class ProcessStartedEvent( timstamp : DateTime, event : EventChain ) extends ProcessEvent
+case class ProcessAdvancedEvent( timestamp : DateTime, event : EventChain ) extends ProcessEvent
+case class ProcessEndedEvent( timestamp : DateTime, event : EventChain ) extends ProcessEvent
 
 
 
