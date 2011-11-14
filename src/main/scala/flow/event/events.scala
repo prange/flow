26e3a329b7b9aeed4c746df8b8c6560fbf51100f
Atle Osmoen-Prange <aop@fourspark.no>
2011-11-14 15:24:44 +0100

    + Interval på EventChain, og events for opprette avansere og avslutte prosesser

--- src/main/scala/flow/event/events.scala
+++ src/main/scala/flow/event/events.scala
@@ -4,15 +4,23 @@ import org.joda.time.DateTime
 import com.codecommit.antixml._
 import scalaz._
 import Scalaz._
+import org.joda.time.Interval
 
 case class XmlEvent( eventTime : DateTime, eventType : String, data : Group[Elem] ) {
 	def select( property : Selector[Elem] ) = data \ property \ text headOption
 }
 
-case class EventChain( events : NonEmptyList[XmlEvent], data : Group[Elem] ) {
-	def ::( event : XmlEvent ) = EventChain( event <:: events, data )
+case class EventChain( id:String,events : NonEmptyList[XmlEvent], data : Group[Elem] ) {
+	def ::( event : XmlEvent ) = EventChain( id, event <:: events, data )
 	def select( property : Selector[Elem] ) = data \ property \ text headOption
+	def interval = if(events.tail.size==0) new Interval(events.head.eventTime.getMillis(),events.head.eventTime.getMillis()+1) else (events.head.eventTime, events.tail.last.eventTime)
 }
+
+object EventChain {
+	
+	def from(idExtractor:XmlEvent=>String, event : XmlEvent ) = EventChain( idExtractor(event),event.wrapNel, Group() )
+}
+
 
 trait TimerEvent{
 	val time:DateTime
@@ -27,10 +35,11 @@ case class DayTimer(time:DateTime) extends TimerEvent
 case class WeekTimer(time:DateTime) extends TimerEvent
 case class MonthTimer(time:DateTime) extends TimerEvent
 
-object EventChain {
 
-	def from( event : XmlEvent ) = EventChain( event.wrapNel, Group() )
-}
+trait ProcessEvent
+case class ProcessStartedEvent( timstamp:DateTime, event : EventChain ) extends ProcessEvent
+case class ProcessAdvancedEvent(timestamp:DateTime,  event : EventChain ) extends ProcessEvent
+case class ProcessEndedEvent( timestamp:DateTime, event : EventChain ) extends ProcessEvent
 
 
 
--- src/main/scala/flow/operator/assemble.scala
+++ src/main/scala/flow/operator/assemble.scala
@@ -5,35 +5,53 @@ import flow.service.Store
 import flow.service.Datastore
 import flow.event.EventChain
 import flow.event.EventChain
+import flow.actor.OperatorState
 
 object Assemble {
 
 }
 
 trait EventChainCut {
-	def apply( event : XmlEvent, eventChain : EventChain ) : ( Option[ProcessStartedEvent], Option[ProcessAdvancedEvent], Option[ProcessEndedEvent] )
+	type Exists[T] = Option[T]
+	def apply( event : XmlEvent, eventChain : Exists[EventChain] ) : ( Option[ProcessStartedEvent], Option[ProcessAdvancedEvent], Option[ProcessEndedEvent] )
 }
 
-case class CutBefore( predicate : Predicate[XmlEvent] ) extends EventChainCut {
+case class CutBefore( idExtractor : XmlEvent ⇒ String, predicate : Predicate[XmlEvent] ) extends EventChainCut {
 
-	def apply( event : XmlEvent, eventChain : EventChain ) : ( Option[ProcessStartedEvent], Option[ProcessAdvancedEvent], Option[ProcessEndedEvent] ) = ( eventChain.events, predicate( event ) ) match {
-		case ( Nil, _ ) ⇒ ( Some( ProcessStartedEvent( EventChain.from( event ) ) ), Some( ProcessAdvancedEvent( EventChain.from( event ) ) ), None )
-		case ( xs, false ) ⇒ ( None, Some( ProcessAdvancedEvent( event :: eventChain ) ), None )
-		case ( xs, true ) ⇒ ( Some( ProcessStartedEvent( EventChain.from( event ) ) ), Some( ProcessAdvancedEvent( EventChain.from( event ) ) ), Some( ProcessEndedEvent( eventChain ) ) )
+	def apply( event : XmlEvent, eventChain : Exists[EventChain] ) : ( Option[ProcessStartedEvent], Option[ProcessAdvancedEvent], Option[ProcessEndedEvent] ) = ( eventChain, predicate( event ) ) match {
+		case ( None, _ ) ⇒ ( Some( ProcessStartedEvent( EventChain.from(idExtractor, event ) ) ), Some( ProcessAdvancedEvent( EventChain.from( event ) ) ), None )
+		case ( Some( chain ), false ) ⇒ ( None, Some( ProcessAdvancedEvent( event :: chain ) ), None )
+		case ( Some( chain ), true ) ⇒ ( Some( ProcessStartedEvent( EventChain.from( idExtractor,event ) ) ), Some( ProcessAdvancedEvent( EventChain.from( event ) ) ), Some( ProcessEndedEvent( chain ) ) )
 	}
 
 }
 
-case class CutAfter( predicate : Predicate[XmlEvent] ) extends EventChainCut {
+case class CutAfter(idExtractor : XmlEvent ⇒ String, predicate : Predicate[XmlEvent] ) extends EventChainCut {
 
-	def apply( event : XmlEvent, eventChain : EventChain ) : ( Option[ProcessStartedEvent], Option[ProcessAdvancedEvent], Option[ProcessEndedEvent] ) = ( eventChain.events, predicate( event ) ) match {
-		case ( Nil, _ ) ⇒ ( Some( ProcessStartedEvent( EventChain.from( event ) ) ), Some( ProcessAdvancedEvent( EventChain.from( event ) ) ), None )
-		case ( xs, false ) ⇒ ( None, Some( ProcessAdvancedEvent( event :: eventChain ) ), None )
-		case ( xs, true ) ⇒ ( None, Some( ProcessAdvancedEvent( event :: eventChain ) ), Some( ProcessEndedEvent( event :: eventChain ) ) )
+	def apply( event : XmlEvent, eventChain : Exists[EventChain] ) : ( Option[ProcessStartedEvent], Option[ProcessAdvancedEvent], Option[ProcessEndedEvent] ) = ( eventChain, predicate( event ) ) match {
+		case ( None, _ ) ⇒ ( Some( ProcessStartedEvent( EventChain.from( event ) ) ), Some( ProcessAdvancedEvent( EventChain.from( event ) ) ), None )
+		case ( Some( chain ), false ) ⇒ ( None, Some( ProcessAdvancedEvent( event :: chain ) ), None )
+		case ( Some( chain ), true ) ⇒ ( None, Some( ProcessAdvancedEvent( event :: chain ) ), Some( ProcessEndedEvent( event :: chain ) ) )
 	}
 
 }
 
-case class ProcessStartedEvent( event : EventChain )
-case class ProcessAdvancedEvent( event : EventChain )
-case class ProcessEndedEvent( event : EventChain )
+trait ProcessEvent
+case class ProcessStartedEvent( event : EventChain ) extends ProcessEvent
+case class ProcessAdvancedEvent( event : EventChain ) extends ProcessEvent
+case class ProcessEndedEvent( event : EventChain ) extends ProcessEvent
+
+case class AssembleOutput( newProcess : Option[ProcessStartedEvent], advanced : Option[ProcessAdvancedEvent], ended : Option[ProcessEndedEvent] ) {
+	def get = ( newProcess, advanced, ended )
+	def update( activeProcesses : Map[String, EventChain] ) : Map[String, EventChain] = {
+
+	}
+}
+
+case class InMemoryAssembleState( cutter : EventChainCut, activeProcesses : Map[String, EventChain], idExtractor : XmlEvent ⇒ String ) extends OperatorState[XmlEvent, ( Option[ProcessStartedEvent], Option[ProcessAdvancedEvent], Option[ProcessEndedEvent] )] {
+
+	def apply( event : XmlEvent ) = {
+
+	}
+
+}
